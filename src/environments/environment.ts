cfde2a6c86eae5cf5411fd8dfb202fff81ae47ec
Vivi <722390@awsrede.corp>
2022-01-21 21:25:57 -0200

    novo endereco

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 
 export const environment = {
   production: false,
-  // baseUrl : "http://ec2-100-26-42-73.compute-1.amazonaws.com:8088"
-  baseUrl : "http://localhost:8088"
+  baseUrl : "http://ec2-54-163-35-173.compute-1.amazonaws.com:9088"
+  // baseUrl : "http://localhost:8088"
 };
