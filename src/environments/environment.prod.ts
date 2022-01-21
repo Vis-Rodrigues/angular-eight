d037517890bbcf7479d1013411a1773260362399
Vivi <722390@awsrede.corp>
2022-01-21 00:07:18 -0200

    alterado url do backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl : "http://ec2-18-205-235-245.compute-1.amazonaws.com:8088"
+  baseUrl : "http://ec2-100-26-42-73.compute-1.amazonaws.com:8088"
   // baseUrl : "http://localhost:8088"
 };
